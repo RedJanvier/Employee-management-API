d637b36bebabb49bf42369cc2bdca4de53ef5759
RedJanvier <janvierntwali@gmail.com>
2020-03-25 16:40:39 +0200

    chore (setup): initial setup
    Separated some of the functions from controllers/employees.js to utils/employees.js
    
    no footer

--- controllers/employees.js
+++ controllers/employees.js
@@ -1,4 +1,3 @@
-const nodemailer = require('nodemailer');
 const { QueryTypes, Op } = require('sequelize');
 const db = require('../config/database').conn;
 const Employee = require('../models/employees');
@@ -9,55 +8,25 @@ const utils = require('../utils/employees');
 // Access   Public
 exports.create = (req, res) => {
     db.sync({ logging: false })
-        .then(async() => {
-
+        .then(async () => {
             try {
-                const employee = await Employee.create(req.body);
-                
-                const config = {
-                    mailserver: {
-                        host: 'smtp.gmail.com',
-                        port: 587,
-                        secure: false,
-                        auth: {
-                            user: process.env.MAIL,
-                            pass: process.env.MAIL_PASS
-                        }
-                    },
-                    mail: {
-                        from: 'manager@company.org',
-                        to: `${employee.email}`,
-                        subject: 'Communication - Joined the Company',
-                        html: `
-                            <p>Dear ${employee.name}, </p>
-                            <p>It is with great pleasure, we inform you that you have successfully joined 
-                            The Company today. We invite you to be part of one of our best team and to favor
-                            hardwork and to be at the office in Rwanda tomorrow morning at 7am.</p>
-                            <p>Looking forward for your active participation.</p>
-                            <p>Regards</p>
-                            <p>Manager</p>`
-                    }
-                };
-                
-                const sendMail = async ({ mailserver, mail }) => {
-                    let transporter = nodemailer.createTransport(mailserver);
-                    let info = await transporter.sendMail(mail);
-                    console.log(`Preview: ${nodemailer.getTestMessageUrl(info)}`);
-                };
-                sendMail(config).catch(console.error);
-                
-                res.status(201).json({
+                console.log(await utils.encryptPassword(req.body.password));
+                const employee = await Employee.create({
+                    ...req.body,
+                    password: await utils.encryptPassword(req.body.password)
+                });
+
+                await utils.sendEmail('communicaiton', employee.email);
+
+                return await res.status(201).json({
                     success: true,
                     message: `Employee ${employee.name} successfully created`
                 });
-
             } catch (error) {
-                
                 await res.status(400).json({
                     success: false,
                     message: error.errors[0].message
                 });
-
             }
         })
         .catch(err => {
@@ -74,23 +43,21 @@ exports.create = (req, res) => {
 exports.delete = async (req, res) => {
     const { uuid } = req.params;
     try {
-
         const employee = await Employee.destroy({ where: { uuid } });
 
-        if(employee === 0) { throw new Error('Employee not found'); }
+        if (employee === 0) {
+            throw new Error('Employee not found');
+        }
 
-        res.status(200).json({ 
+        res.status(200).json({
             success: true,
             message: `${employee} Employees successfully deleted`
         });
-
-    } catch(err) {
-
+    } catch (err) {
         res.status(500).json({
             success: false,
             message: err.message
         });
-        
     }
 };
 
@@ -102,49 +69,54 @@ exports.edit = (req, res) => {
     const { body } = req;
 
     db.sync({ logging: false })
-    .then(async() => {
-        try {
-    
-            const employee = await Employee.update(body, { where: { uuid } });
-            
-            if (!employee[0]) { throw new Error('Employee not created'); }
+        .then(async () => {
+            try {
+                const employee = await Employee.update(body, {
+                    where: { uuid }
+                });
 
-            await res.status(200).json({ 
-                success: true,
-                message: `Employee successfully modified`
-            });
-            
-        } catch (error) {
-            
-            await res.status(304).json({ 
+                if (!employee[0]) {
+                    throw new Error('Employee not created');
+                }
+
+                await res.status(200).json({
+                    success: true,
+                    message: `Employee successfully modified`
+                });
+            } catch (error) {
+                await res.status(304).json({
+                    success: false,
+                    message: error.message
+                });
+            }
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
                 success: false,
-                message: error.message
+                message: 'Error occurred on the server; Employee not modified'
             });
-        }
-    })
-    .catch(err => {
-        console.log(err);
-        res.status(500).json({
-            success: false,
-            message: 'Error occurred on the server; Employee not modified'
         });
-    });
 };
 
 // @desc    Suspend/Activate an employee
 // Route    PUT /api/v1/employees/:id/:status
 // Access   Public
-exports.status = (req, res )=> {
+exports.status = (req, res) => {
     let { uuid, status } = req.params;
-    if (status === 'activate' || status === 'suspend'){
+    if (status === 'activate' || status === 'suspend') {
         try {
-            db.query(`UPDATE employees SET status = :status WHERE employees.uuid = :uuid`, { 
-            replacements: { 
-                status: (status === 'activate')? 'active' : 'inactive',
-                uuid: uuid
-            }, type: QueryTypes.UPDATE })
-            .then(() => {
-                (status === 'suspend')? status = 'suspende': '';
+            db.query(
+                `UPDATE employees SET status = :status WHERE employees.uuid = :uuid`,
+                {
+                    replacements: {
+                        status: status === 'activate' ? 'active' : 'inactive',
+                        uuid: uuid
+                    },
+                    type: QueryTypes.UPDATE
+                }
+            ).then(() => {
+                status === 'suspend' ? (status = 'suspende') : '';
                 res.status(201).json({
                     success: true,
                     message: `Employee was ${status}d successfully`
@@ -171,21 +143,23 @@ exports.status = (req, res )=> {
 exports.search = (req, res) => {
     Object.keys(req.query).map(async q => {
         try {
-            
             let where = {};
             where[q] = {
                 [Op.like]: `%${req.query[q]}%`
             };
 
             const employees = await Employee.findAll({ where });
-            
-            if (employees.length < 1) throw new Error(`No employee found of that ${q}`);
 
+            if (employees.length < 1)
+                throw new Error(`No employee found of that ${q}`);
+            // console.log(employees);
             await res.status(200).json({
                 success: true,
-                employees
+                data: employees.map(employee => ({
+                    ...employee.dataValues,
+                    password: null
+                }))
             });
-
         } catch (error) {
             res.status(404).json({
                 success: false,
--- utils/employees.js
+++ utils/employees.js
@@ -1,6 +1,52 @@
-exports.checkAge = birthday => { // birthday is a date
+const nodemailer = require('nodemailer');
+const bcrypt = require('bcrypt');
+
+const sendMail = async ({ mailserver, mail }) => {
+    let transporter = nodemailer.createTransport(mailserver);
+    let info = await transporter.sendMail(mail);
+    console.log(`Preview: ${nodemailer.getTestMessageUrl(info)}`);
+};
+
+exports.checkAge = birthday => {
+    // birthday is a date
     const ageDifMs = Date.now() - birthday.getTime();
     const ageDate = new Date(ageDifMs); // miliseconds from epoch
     const age = Math.abs(ageDate.getUTCFullYear() - 1970);
     return age;
-}
+};
+
+exports.sendEmail = async (type, to) => {
+    let mail = {
+        from: 'manager@company.org',
+        to,
+        subject: `${type} - Joined the Company`,
+        html: `
+            <p>Dear ${to}, </p>
+            <p>It is with great pleasure, we inform you that you have successfully joined 
+            The Company today. We invite you to be part of one of our best team and to favor
+            hardwork and to be at the office in Rwanda tomorrow morning at 7am.</p>
+            <p>Looking forward for your active participation.</p>
+            <p>Regards</p>
+            <p>Manager</p>`
+    };
+    const config = {
+        mailserver: {
+            host: 'smtp.gmail.com',
+            port: 587,
+            secure: false,
+            auth: {
+                user: process.env.MAIL,
+                pass: process.env.MAIL_PASS
+            }
+        },
+        mail
+    };
+
+    return await sendMail(config).catch(console.error);
+};
+
+exports.encryptPassword = async password => {
+    const salt = await bcrypt.genSalt(12);
+    const hash = await bcrypt.hash(password, salt);
+    return hash;
+};
